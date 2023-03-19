2c00367012dafad4e8e2c26fce85f6110a5cd6a2
rodionion <rodionvh@gmail.com>
2023-03-19 11:28:11 -0300

    Change styling

--- src/pages/UnderConstructionPage.js
+++ src/pages/UnderConstructionPage.js
@@ -8,11 +8,8 @@ export default function UnderConstructionPage() {
                     <h1>
                         Coming soon..
                     </h1>
-                    {/* <h2> */}
-                        {/* Our website is under construction */}
-                    {/* </h2> */}
                     <p>
-                        We are working hard to open The Sunningdale Restaurant to serve you with the best experience in Lusaka.
+                        We are working hard to open The Sunningdale House to serve you with the best experience in Lusaka.
                     </p>
                     <p>
                         The Sunningdale House © 2023. All Rights Reserved.
